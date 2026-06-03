11bc7112fe3f96b1595f9a9a1b8700417d72c2c1
mohamedesz40-source <mohamedesz40@gmail.com>
2026-06-03 23:45:21 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 module.exports = {
     bot: {
         tokens: [
-          "YOUR_BOT_TOKEN_HERE",              // Main bot token - Replace with your actual token
-        //  "YOUR_SECOND_BOT_TOKEN_HERE",      // 2 bot token - Uncomment and replace to use
-        //  "YOUR_THIRD_BOT_TOKEN_HERE",       // 3 bot token - Uncomment and replace to use
-        //  "YOUR_FOURTH_BOT_TOKEN_HERE",      // 4 bot token - Uncomment and replace to use
-        //  "YOUR_FIFTH_BOT_TOKEN_HERE",       // 5 bot token - Uncomment and replace to use
+          "MTUxMTgyMzY3ODk2NTQyMDA4Mg.Gr0Ek1.P22dsX6IbZad9x7awpzvZphxNXj_CsTg-VvSgE",              // Main bot token - Replace with your actual token
+        //  "MTUxMTgyNzA1ODk0MTIzNTMwMg.Gh-eZa.x7la9vwDDj6QHuzPjDiFCJ4brcfN0NUBZwlqUk",      // 2 bot token - Uncomment and replace to use
+        //  "MTUxMTgyNzIyNjE4MDU4NzcyMQ.GPi6li.A7huyww5Fu7o-2z1XIHmT8SSlXgJRDQZJIez6s",       // 3 bot token - Uncomment and replace to use
+        //  "MTUxMTgyNzM2MjAwNDU5ODgxNA.GPNGSm.piqncBrfaN6ccFsUVeZcP9sGM9zrfI2rueENYA",      // 4 bot token - Uncomment and replace to use
+        //  "MTUxMTgyNzQ4OTYwMzk3NzQ1Nw.G9YLJE.GPdwH_WRwjIn3OjCsmFdn0f8vUgBVczuPHUyec",       // 5 bot token - Uncomment and replace to use
         ].filter(Boolean),
 
         defaultLanguage: 'ar', // ar | en لغة البوت
@@ -17,9 +17,9 @@ module.exports = {
         }
     },
     server: {
-        guildId: 'ID',         // ايدي السيرفر
-        broadcastRoleId: 'ID', // ايدي الرول اللي يستخدم عليها البوت
-        reportChannelId: 'ID' // ايدي الروم اللي يرسل فيه البوت التقارير
+        guildId: '1145083762552619048',         // ايدي السيرفر
+        broadcastRoleId: '1479826329665798388', // ايدي الرول اللي يستخدم عليها البوت
+        reportChannelId: '1145083762997203014' // ايدي الروم اللي يرسل فيه البوت التقارير
     },
     broadcast: {
         cooldownTime: 1000, // لا تلعب فيها
